5a17eaed4ec9528c3e7121b6911524489b4f9f3a
Matth_eu <matthew@matth.eu>
2014-10-15 16:41:25 +0100

    Better templates

--- inc/templates/shortcode-blockquote-render-js.tpl.php
+++ inc/templates/shortcode-blockquote-render-js.tpl.php
@@ -1,6 +1,6 @@
 <script type="text/html" id="tmpl-shortcode-blockquote-render-js">
 	<blockquote>
 		{{ data.content }}
-		<small>{{ data.source }}</small>
+		<br/><small>{{ data.source }}</small>
 	</blockquote>
 </script>
