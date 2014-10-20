ab15917ef08aaa7ccc8a2d4c14d16a83269c36e8
Matth_eu <matthew@matth.eu>
2014-10-20 19:17:24 +0100

    update plugin header

--- shortcode-ui.php
+++ shortcode-ui.php
@@ -1,28 +1,22 @@
 <?php
 /**
  * Plugin Name: Shortcode UI
- * Version: 0.1-beta
- * Description: Interface for adding shortcodes.
+ * Version: 1.0-alpha
+ * Description: User Interface for adding shortcodes.
  * Author: Human Made Limited
  * Author URI: http://hmn.md
+ * Text Domain: shortcode-ui
+ * License: GPL v2 or later
  *
- * License: GPLv2 or later
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- *	Copyright (C) 2013 Dominik Schilling
- *
- *	This program is free software; you can redistribute it and/or
- *	modify it under the terms of the GNU General Public License
- *	as published by the Free Software Foundation; either version 2
- *	of the License, or (at your option) any later version.
- *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *	GNU General Public License for more details.
- *
- *	You should have received a copy of the GNU General Public License
- *	along with this program; if not, write to the Free Software
- *	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 require_once( 'inc/class-shortcode-ui.php' );
