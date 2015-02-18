9d3d0892a7d06ad9aa9f0a22563b016afe745c36
Benjamin Intal <bfintal@gmail.com>
2015-02-18 10:46:07 +0800

    Removed color from the default list of fields

--- inc/fields/class-shortcode-ui-fields.php
+++ inc/fields/class-shortcode-ui-fields.php
@@ -37,9 +37,6 @@ class Shortcode_UI_Fields {
 		'date' => array(
 			'template' => 'shortcode-ui-field-date',
 		),
-		'color' => array(
-			'template' => 'shortcode-ui-field-color',
-		),
 	);
 
 	public static function get_instance() {
