4da7ed37d3111f325cf5a04828249c72c601bf0e
Matth_eu <matthew@matth.eu>
2015-02-20 12:17:40 +0000

    Run content through _wp_Nop

--- js/shortcode-ui.js
+++ js/shortcode-ui.js
@@ -101,6 +101,7 @@ var Shortcode_UI;
 				// Handle content attribute as a special case.
 				if ( attr.get( 'attr' ) === 'content' ) {
 					content = attr.get( 'value' );
+					content = switchEditors._wp_Nop( content );
 				} else {
 					attrs.push( attr.get( 'attr' ) + '="' + attr.get( 'value' ) + '"' );
 				}
@@ -902,7 +903,7 @@ var Shortcode_UI;
 			if ( matches[3] ) {
 				var content = currentShortcode.get( 'attrs' ).findWhere( { attr: 'content' } );
 				if ( content ) {
-					content.set( 'value', matches[3] );
+					content.set( 'value', switchEditors._wp_Nop( matches[3] ) );
 				}
 			}
 
