476aa1d862af84e49f566b8820d9ac78a6f8663f
Matth_eu <matthew@matth.eu>
2015-03-09 00:28:37 +0000

    update model tests

--- js-tests/src/shortcodeModelSpec.js
+++ js-tests/src/shortcodeModelSpec.js
@@ -3,9 +3,11 @@ var InnerContent = require('../../js/models/inner-content');
 var ShortcodeAttribute = require('../../js/models/shortcode-attribute');
 var ShortcodeAttributes = require('../../js/collections/shortcode-attributes');
 
-describe( "Shortcode Model Tests", function() {
+describe( "Shortcode Model", function() {
 
-	var data = {
+	var defaultShortcode, shortcode, data;
+
+	data = {
 		label: 'Test Label',
 		shortcode_tag: 'test_shortcode',
 		attrs: [
@@ -22,22 +24,32 @@ describe( "Shortcode Model Tests", function() {
 		},
 	};
 
+	var defaultShortcode = new Shortcode();
 	var shortcode = new Shortcode( data );
 
-	it( 'Basic Data should be set correctly..', function() {
-		expect( shortcode.get( 'label' ) ).toEqual( data.label );
-		expect( shortcode.get( 'shortcode_tag' ) ).toEqual( data.shortcode_tag );
+	it( 'Defaults set correctly.', function() {
+		expect( defaultShortcode.get( 'label' ) ).toEqual( '' );
+		expect( defaultShortcode.get( 'shortcode_tag' ) ).toEqual( '' );
+		expect( defaultShortcode.get( 'attrs' ) instanceof ShortcodeAttributes ).toEqual( true );
+		expect( defaultShortcode.get( 'attrs' ).length ).toEqual( 0 );
+		expect( defaultShortcode.get( 'inner_content' ) instanceof InnerContent ).toEqual( true );
 	});
 
-	it( 'Attribute data should be set correctly..', function() {
+	it( 'Attribute data set correctly..', function() {
 		expect( shortcode.get( 'attrs' ) instanceof ShortcodeAttributes ).toEqual( true );
 		expect( shortcode.get( 'attrs' ).length ).toEqual( 1 );
 		expect( shortcode.get( 'attrs' ).first().get('type') ).toEqual( data.attrs[0].type );
 	});
 
-	it( 'Inner content should be set correctly..', function() {
+	it( 'Inner content set correctly..', function() {
 		expect( shortcode.get( 'inner_content' ) instanceof InnerContent ).toEqual( true );
 		expect( shortcode.get( 'inner_content' ).get('value') ).toEqual( data.inner_content.value );
+	});
+
+	it( 'Converted to JSON correctly.', function() {
+		var json = shortcode.toJSON();
+		expect( json.label ).toEqual( data.label );
+		expect( json.attrs[0].label ).toEqual( data.attrs[0].label );
 	});
 
 });
