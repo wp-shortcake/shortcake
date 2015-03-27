09763a2b6582fa0215ff19fb73d1a92f0c3a9f8c
Greg Ross <greg@toolstack.com>
2015-03-27 16:36:02 -0400

    - Added size, rows, cols to options for template fields. - Added required option to template fields to add a * after the label.

--- inc/templates/edit-form.tpl.php
+++ inc/templates/edit-form.tpl.php
@@ -17,8 +17,8 @@
 
 <script type="text/html" id="tmpl-shortcode-ui-field-text">
 	<div class="field-block">
-		<label for="{{ data.attr }}">{{ data.label }}</label>
-		<input type="text" name="{{ data.attr }}" id="{{ data.attr }}" value="{{ data.value }}" placeholder="{{ data.placeholder }}"/>
+		<label for="{{ data.attr }}">{{ data.label }}<# if ( data.required == true ) { #> *<# } #></label>
+		<input type="text" name="{{ data.attr }}" id="{{ data.attr }}" value="{{ data.value }}"  size="{{ data.size }}" placeholder="{{ data.placeholder }}"/>
 		<# if ( typeof data.description == 'string' ) { #>
 			<p class="description">{{ data.description }}</p>
 		<# } #>
@@ -27,8 +27,8 @@
 
 <script type="text/html" id="tmpl-shortcode-ui-field-url">
 	<div class="field-block">
-		<label for="{{ data.attr }}">{{ data.label }}</label>
-		<input type="url" name="{{ data.attr }}" id="{{ data.attr }}" value="{{ data.value }}" class="code" placeholder="{{ data.placeholder }}"/>
+		<label for="{{ data.attr }}">{{ data.label }}<# if ( data.required == true ) { #> *<# } #></label>
+		<input type="url" name="{{ data.attr }}" id="{{ data.attr }}" value="{{ data.value }}" size="{{ data.size }}" class="code" placeholder="{{ data.placeholder }}"/>
 		<# if ( typeof data.description == 'string' ) { #>
 			<p class="description">{{ data.description }}</p>
 		<# } #>
@@ -37,8 +37,8 @@
 
 <script type="text/html" id="tmpl-shortcode-ui-field-textarea">
 	<div class="field-block">
-		<label for="{{ data.attr }}">{{ data.label }}</label>
-		<textarea name="{{ data.attr }}" id="{{ data.attr }}" placeholder="{{ data.placeholder }}">{{ data.value }}</textarea>
+		<label for="{{ data.attr }}">{{ data.label }<# if ( data.required == true ) { #> *<# } #>}</label>
+		<textarea name="{{ data.attr }}" id="{{ data.attr }}" cols="{{ data.cols }}" rows="{{ data.rows }}" placeholder="{{ data.placeholder }}">{{ data.value }}</textarea>
 		<# if ( typeof data.description == 'string' ) { #>
 			<p class="description">{{ data.description }}</p>
 		<# } #>
@@ -47,7 +47,7 @@
 
 <script type="text/html" id="tmpl-shortcode-ui-field-select">
 	<div class="field-block">
-		<label for="{{ data.attr }}">{{ data.label }}</label>
+		<label for="{{ data.attr }}">{{ data.label }}<# if ( data.required == true ) { #> *<# } #></label>
 		<select name="{{ data.attr }}" id="{{ data.attr }}">
 			<# _.each( data.options, function( label, value ) { #>
 				<option value="{{ value }}" <# if ( value == data.value ){ print('selected'); } #>>{{ label }}</option>
@@ -61,7 +61,7 @@
 
 <script type="text/html" id="tmpl-shortcode-ui-field-radio">
 	<div class="field-block">
-		<label for="{{ data.attr }}">{{ data.label }}</label>
+		<label for="{{ data.attr }}">{{ data.label }}<# if ( data.required == true ) { #> *<# } #></label>
 		<# _.each( data.options, function( label, value ) { #>
 			<input type="radio" name="{{ data.attr }}" value="{{ value }}" <# if ( value == data.value ){ print('checked'); } #>>{{ label }}<br />
 		<# }); #>
@@ -75,7 +75,7 @@
 	<div class="field-block">
 		<label for="{{ data.attr }}">
 			<input type="checkbox" name="{{ data.attr }}" id="{{ data.attr }}" value="{{ data.value }}" <# if ( 'true' == data.value ){ print('checked'); } #>>
-			{{ data.label }}
+			{{ data.label }}<# if ( data.required == true ) { #> *<# } #>
 		</label>
 		<# if ( typeof data.description == 'string' ) { #>
 			<p class="description">{{ data.description }}</p>
@@ -85,8 +85,8 @@
 
 <script type="text/html" id="tmpl-shortcode-ui-field-email">
 	<div class="field-block">
-		<label for="{{ data.attr }}">{{ data.label }}</label>
-		<input type="email" name="{{ data.attr }}" id="{{ data.attr }}" value="{{ data.value}}" placeholder="{{ data.placeholder }}"/>
+		<label for="{{ data.attr }}">{{ data.label }}<# if ( data.required == true ) { #> *<# } #></label>
+		<input type="email" name="{{ data.attr }}" id="{{ data.attr }}" value="{{ data.value}}" size="{{ data.size }}" placeholder="{{ data.placeholder }}"/>
 		<# if ( typeof data.description == 'string' ) { #>
 			<p class="description">{{ data.description }}</p>
 		<# } #>
@@ -95,8 +95,8 @@
 
 <script type="text/html" id="tmpl-shortcode-ui-field-number">
 	<div class="field-block">
-		<label for="{{ data.attr }}">{{ data.label }}</label>
-		<input type="number" name="{{ data.attr }}" id="{{ data.attr }}" value="{{ data.value}}" placeholder="{{ data.placeholder }}"/>
+		<label for="{{ data.attr }}">{{ data.label }}<# if ( data.required == true ) { #> *<# } #></label>
+		<input type="number" name="{{ data.attr }}" id="{{ data.attr }}" value="{{ data.value}}" size="{{ data.size }}" placeholder="{{ data.placeholder }}"/>
 		<# if ( typeof data.description == 'string' ) { #>
 			<p class="description">{{ data.description }}</p>
 		<# } #>
@@ -115,7 +115,7 @@
 
 <script type="text/html" id="tmpl-shortcode-ui-field-date">
 	<div class="field-block">
-		<label for="{{ data.attr }}">{{ data.label }}</label>
+		<label for="{{ data.attr }}">{{ data.label }}<# if ( data.required == true ) { #> *<# } #></label>
 		<input type="date" name="{{ data.attr }}" id="{{ data.attr }}" value="{{ data.value }}" placeholder="{{ data.placeholder }}"/>
 		<# if ( typeof data.description == 'string' ) { #>
 			<p class="description">{{ data.description }}</p>
@@ -125,8 +125,8 @@
 
 <script type="text/html" id="tmpl-shortcode-ui-content">
 	<div class="field-block">
-		<label for="inner_content">{{ data.label }}</label>
-		<textarea id="inner_content" name="inner_content" class="content-edit">{{ data.value }}</textarea>
+		<label for="inner_content">{{ data.label }}<# if ( data.required == true ) { #> *<# } #></label>
+		<textarea id="inner_content" name="inner_content" cols="{{ data.cols }}" rows="{{ data.rows }}" class="content-edit">{{ data.value }}</textarea>
 		<# if ( typeof data.description == 'string' ) { #>
 			<p class="description">{{ data.description }}</p>
 		<# } #>
