abd32f59f5e4b31317fda85c25e462c0ec64363b
Daniel Bachhuber <d@danielbachhuber.com>
2015-04-08 09:20:06 -0700

    Tag v0.2.3

--- shortcode-ui.php
+++ shortcode-ui.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Shortcode UI
- * Version: v0.2.2
+ * Version: v0.2.3
  * Description: User Interface for adding shortcodes.
  * Author: Fusion Engineering and community
  * Author URI: http://next.fusion.net/tag/shortcode-ui/
@@ -19,7 +19,7 @@
  * GNU General Public License for more details.
  */
 
-define( 'SHORTCODE_UI_VERSION', '0.2.2' );
+define( 'SHORTCODE_UI_VERSION', '0.2.3' );
 
 require_once dirname( __FILE__ ) . '/inc/class-shortcode-ui.php';
 require_once dirname( __FILE__ ) . '/inc/fields/class-shortcode-ui-fields.php';
