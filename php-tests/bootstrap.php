fc2b041e3d627a7c76ac2f8dd853c75071c5ab08
Daniel Bachhuber <d@danielbachhuber.com>
2015-04-21 14:13:05 -0700

    Fix coding standards in bootstrap file

--- php-tests/bootstrap.php
+++ php-tests/bootstrap.php
@@ -1,12 +1,14 @@
 <?php
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
 
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../shortcode-ui.php';
+	include dirname( __FILE__ ) . '/../shortcode-ui.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
