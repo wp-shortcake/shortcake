c054ae94589ef4b90ee0abc1e792a98d608b6b9b
Matth_eu <matthew@matth.eu>
2015-04-21 14:49:10 +0100

    revert shortcode regex

--- inc/class-shortcode-ui.php
+++ inc/class-shortcode-ui.php
@@ -97,7 +97,6 @@ class Shortcode_UI {
 		wp_enqueue_style( 'shortcode-ui', $this->plugin_url . 'css/shortcode-ui.css', array(), $this->plugin_version );
 		wp_localize_script( 'shortcode-ui', ' shortcodeUIData', array(
 			'shortcodes'      => $shortcodes,
-			'shortcode_regex' => get_shortcode_regex(),
 			'strings'         => array(
 				'media_frame_title'                => esc_html__( 'Insert Post Element', 'shortcode-ui' ),
 				'media_frame_menu_insert_label'    => esc_html__( 'Insert Post Element', 'shortcode-ui' ),
