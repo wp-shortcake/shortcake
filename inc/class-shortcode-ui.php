f15d87dbe8862a44e1bacb2dcb502d361bfad8bd
Daniel Bachhuber <d@danielbachhuber.com>
2015-07-28 10:12:40 -0700

    PHPdoc for the `Shortcode_UI` class

--- inc/class-shortcode-ui.php
+++ inc/class-shortcode-ui.php
@@ -1,37 +1,81 @@
 <?php
 
+/**
+ * Primary controller class for Shortcake
+ */
 class Shortcode_UI {
 
+	/**
+	 * Plugin directory
+	 *
+	 * @access private
+	 * @var string
+	 */
 	private $plugin_dir;
+
+	/**
+	 * Plugin URL
+	 *
+	 * @access private
+	 * @var string
+	 */
 	private $plugin_url;
 
+	/**
+	 * Shortcodes with registered shortcode UI
+	 *
+	 * @access private
+	 * @var array
+	 */
 	private $shortcodes = array();
 
+	/**
+	 * Shortcake controller instance
+	 *
+	 * @access private
+	 * @var object
+	 */
 	private static $instance;
 
+	/**
+	 * Get instance of Shortcake controller
+	 * Instantiates object on the fly when not
+	 * already loaded
+	 *
+	 * @return object
+	 */
 	public static function get_instance() {
 		if ( ! isset( self::$instance ) ) {
 			self::$instance = new self;
 			self::$instance->setup_actions();
 		}
-
 		return self::$instance;
 	}
 
-	function __construct() {
-
+	/**
+	 * Set up instance attributes when initially loaded
+	 */
+	private function __construct() {
 		$this->plugin_version = SHORTCODE_UI_VERSION;
 		$this->plugin_dir     = plugin_dir_path( dirname( __FILE__ ) );
 		$this->plugin_url     = plugin_dir_url( dirname( __FILE__ ) );
-
 	}
 
+	/**
+	 * Setup plugin actions
+	 */
 	private function setup_actions() {
 		add_action( 'admin_enqueue_scripts', array( $this, 'action_admin_enqueue_scripts' ) );
 		add_action( 'wp_enqueue_editor',     array( $this, 'action_wp_enqueue_editor' ) );
 		add_action( 'wp_ajax_do_shortcode',  array( $this, 'handle_ajax_do_shortcode' ) );
 	}
 
+	/**
+	 * Register UI for a shortcode
+	 *
+	 * @param string $shortcode_tag Tag used for the shortcode
+	 * @param array $args Configuration parameters for shortcode UI
+	 */
 	public function register_shortcode_ui( $shortcode_tag, $args = array() ) {
 
 		// inner_content=true is a valid argument, but we want more detail
@@ -51,27 +95,40 @@ class Shortcode_UI {
 
 	}
 
+	/**
+	 * Get configuration parameters for all shortcodes with UI
+	 *
+	 * @return array
+	 */
 	public function get_shortcodes() {
 		return apply_filters( 'shortcode_ui_shortcodes', $this->shortcodes );
 	}
 
+	/**
+	 * Get UI configuration parameters for a given shortcode
+	 *
+	 * @return array|false
+	 */
 	public function get_shortcode( $shortcode_tag ) {
 
 		$shortcodes = $this->get_shortcodes();
 		if ( isset( $shortcodes[ $shortcode_tag ] ) ) {
 			return $shortcodes[ $shortcode_tag ];
 		}
-
+		return false;
 	}
 
 	/**
-	 * Action admin scripts.
+	 * Enqueue scripts and styles used in the admin
 	 */
 	public function action_admin_enqueue_scripts() {
 		// Editor styles needs to be added before wp_enqueue_editor
 		add_editor_style( trailingslashit( $this->plugin_url ) . 'css/shortcode-ui-editor-styles.css' );
 	}
 
+	/**
+	 * Enqueue scripts and styles needed for shortcode UI
+	 */
 	public function enqueue() {
 
 		if ( did_action( 'enqueue_shortcode_ui' ) ) {
@@ -124,14 +181,14 @@ class Shortcode_UI {
 			),
 		) );
 
-		// queue templates
+		// add templates to the footer, instead of where we're at now
 		add_action( 'admin_print_footer_scripts', array( $this, 'action_admin_print_footer_scripts' ) );
 
 		do_action( 'enqueue_shortcode_ui' );
 	}
 
 	/**
-	 * Default hook to queue shortcake from
+	 * Enqueue shortcode UI assets when the editor is enqueued
 	 */
 	public function action_wp_enqueue_editor() {
 		// queue scripts & templates
@@ -141,9 +198,7 @@ class Shortcode_UI {
 	}
 
 	/**
-	 * Output required underscore.js templates
-	 *
-	 * @return null
+	 * Output required underscore.js templates in the footer
 	 */
 	public function action_admin_print_footer_scripts() {
 		echo $this->get_view( 'media-frame' ); // WPCS: xss ok
@@ -179,7 +234,7 @@ class Shortcode_UI {
 	}
 
 	/**
-	 * Compare shortcodes by label
+	 * Sort labels alphabetically
 	 *
 	 * @param array $a
 	 * @param array $b
@@ -190,10 +245,7 @@ class Shortcode_UI {
 	}
 
 	/**
-	 * Output a shortcode.
-	 * ajax callback for displaying the shortcode in the TinyMCE editor.
-	 *
-	 * @return null
+	 * Render a shortcode for preview in the TinyMCE editor.
 	 */
 	public function handle_ajax_do_shortcode() {
 
