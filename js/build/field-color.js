03d9f40ab402990a13a55c47b6563644b3fa49a2
davisshaver <davis@daviss-MacBook.local>
2015-08-05 15:00:13 -0400

    Pass false shortcake value for checkboxes

--- js/build/field-color.js
+++ js/build/field-color.js
@@ -165,8 +165,8 @@ Shortcode = Backbone.Model.extend({
 
 		this.get( 'attrs' ).each( function( attr ) {
 
-			// Skip empty attributes.
-			if ( ! attr.get( 'value' ) ||  attr.get( 'value' ).length < 1 ) {
+			// Skip empty attributes
+			if ( ( ! attr.get( 'value' ) ||  attr.get( 'value' ).length < 1 )  && ( attr.attributes.type != 'checkbox' ) ) {
 				return;
 			}
 
