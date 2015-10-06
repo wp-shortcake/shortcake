09bd2b9974f682e83a60e2f657396d1d9f86e5ee
Matth_eu <matthew@matth.eu>
2015-10-06 17:12:18 +0100

    PHP test attribute decoding

--- php-tests/test-shortcode-ui.php
+++ php-tests/test-shortcode-ui.php
@@ -13,4 +13,33 @@ class Test_Shortcode_UI extends WP_UnitTestCase {
 		$this->assertArrayHasKey( 'post_select', $fields );
 	}
 
+	public function test_filter_shortcode_atts_decode_encoded() {
+
+		$shortcode = 'shortcake_encoding_test';
+
+		// Add test Shortcode.
+		add_shortcode( $shortcode, '__return_null' );
+
+		// Register test Shortcode UI.
+		shortcode_ui_register_for_shortcode( $shortcode, array(
+			'attrs' => array(
+				array(
+					'attr'    => 'test',
+					'type'    => 'text',
+					'encoded' => true,
+				),
+			),
+		) );
+
+		$encoded = '%3Cb%20class%3D%22foo%22%3Ebar%3C%2Fb%3E';
+		$decoded = '<b class="foo">bar</b>';
+
+		// Parse shortcode attributes.
+		$attr = shortcode_atts( array(), array( 'test' => $encoded ), $shortcode );
+
+		// Expect value of $attr['test'] to be decoded.
+		$this->assertEquals( $attr['test'], $decoded );
+
+	}
+
 }
