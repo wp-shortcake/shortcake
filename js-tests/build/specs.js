1a98c853532c4786534fb735de0271d761f2f1fc
Matth_eu <matthew@matth.eu>
2015-10-06 15:32:50 +0100

    Don't set a default for encoded from shortcodeUIFieldData

--- js-tests/build/specs.js
+++ js-tests/build/specs.js
@@ -482,15 +482,6 @@ var ShortcodeAttribute = Backbone.Model.extend({
 		},
 	},
 
-	initialize: function() {
-
-		// If value not encoded, set to the default for this field type.
-		if ( ! this.get( 'encoded' ) && shortcodeUIFieldData[ this.get( 'type' ) ] ) {
-			this.set( 'encoded', shortcodeUIFieldData[ this.get( 'type' ) ].encoded );
-		}
-
-	},
-
 });
 
 module.exports = ShortcodeAttribute;
