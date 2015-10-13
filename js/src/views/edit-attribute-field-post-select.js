0228c35163783c1d8d2716727eed5b7f6953dfa7
Daniel Bachhuber <d@danielbachhuber.com>
2015-10-13 15:31:24 -0700

    Update JS docs for editAttributeFieldPostSelect

--- js/src/views/edit-attribute-field-post-select.js
+++ js/src/views/edit-attribute-field-post-select.js
@@ -1,194 +1,207 @@
-( function( $ ) {
+var sui = require('sui-utils/sui'),
+	$ = require('jquery');
 
-	var sui = window.Shortcode_UI;
+// Cached Data.
+var postSelectCache = {};
 
-	// Cached Data.
-	var postSelectCache = {};
+/**
+ * sui.views.editAttributeFieldPostSelect
+ *
+ * The controller for the Post Select Field.
+ *
+ * @class
+ * @augments sui.views.editAttributeField
+ */
+sui.views.editAttributeFieldPostSelect = sui.views.editAttributeField.extend( {
 
-	sui.views.editAttributeFieldPostSelect = sui.views.editAttributeField.extend( {
+	events: {
+		'change .shortcode-ui-post-select': 'inputChanged',
+	},
 
-		events: {
-			'change .shortcode-ui-post-select': 'inputChanged',
-		},
+	/**
+	 * Update saved value and trigger callbacks when input changes
+	 */
+	inputChanged: function(e) {
+		this.setValue( e.val );
+		this.triggerCallbacks();
+	},
 
-		inputChanged: function(e) {
-			this.setValue( e.val );
-			this.triggerCallbacks();
-		},
+	/**
+	 * Render the Post Select field
+	 */
+	render: function() {
 
-		render: function() {
+		var self = this,
+		    defaults = { multiple: false };
 
-			var self = this,
-			    defaults = { multiple: false };
-
-			for ( var arg in defaults ) {
-				if ( ! this.model.get( arg ) ) {
-					this.model.set( arg, defaults[ arg ] );
-				}
+		for ( var arg in defaults ) {
+			if ( ! this.model.get( arg ) ) {
+				this.model.set( arg, defaults[ arg ] );
 			}
+		}
 
-			var data = this.model.toJSON();
-			data.id = 'shortcode-ui-' + this.model.get( 'attr' ) + '-' + this.model.cid;
+		var data = this.model.toJSON();
+		data.id = 'shortcode-ui-' + this.model.get( 'attr' ) + '-' + this.model.cid;
 
-			this.$el.html( this.template( data ) );
+		this.$el.html( this.template( data ) );
 
-			var ajaxData = {
-				action    : 'shortcode_ui_post_field',
-				nonce     : shortcodeUiPostFieldData.nonce,
-				shortcode : this.shortcode.get( 'shortcode_tag'),
-				attr      : this.model.get( 'attr' )
-			};
+		var ajaxData = {
+			action    : 'shortcode_ui_post_field',
+			nonce     : shortcodeUiPostFieldData.nonce,
+			shortcode : this.shortcode.get( 'shortcode_tag'),
+			attr      : this.model.get( 'attr' )
+		};
 
-			var $field = this.$el.find( '.shortcode-ui-post-select' );
+		var $field = this.$el.find( '.shortcode-ui-post-select' );
 
-			$field.select2({
+		$field.select2({
 
-				placeholder: "Search",
-				multiple: this.model.get( 'multiple' ),
-				ajax: {
-					url: ajaxurl,
-					dataType: 'json',
-					quietMillis: 250,
-					data: function (term, page) {
-						ajaxData.s    = term;
-						ajaxData.page = page;
-						return ajaxData;
-					},
-					results: function ( response, page ) {
+			placeholder: "Search",
+			multiple: this.model.get( 'multiple' ),
+			ajax: {
+				url: ajaxurl,
+				dataType: 'json',
+				quietMillis: 250,
+				data: function (term, page) {
+					ajaxData.s    = term;
+					ajaxData.page = page;
+					return ajaxData;
+				},
+				results: function ( response, page ) {
+
+					if ( ! response.success ) {
+						return { results: {}, more: false };
+					}
+
+					// Cache data for quicker rendering later.
+					postSelectCache = $.extend( postSelectCache, response.data.posts );
+
+					var more = ( page * response.data.posts_per_page ) < response.data.found_posts; // whether or not there are more results available
+					return { results: response.data.posts, more: more };
+
+				},
+			},
+
+			/**
+			 * Initialize Callback
+			 * Used to set render the initial value.
+			 * Has to make a request to get the title for the current ID.
+			 */
+			initSelection: function(element, callback) {
+
+				var ids, parsedData = [], cached;
+
+				// Convert stored value to array of IDs (int).
+				ids = $(element)
+					.val()
+					.split(',')
+					.map( function (str) { return str.trim(); } )
+					.map( function (str) { return parseInt( str ); } );
+
+				if ( ids.length < 1 ) {
+					return;
+				}
+
+				// Check if there is already cached data.
+				for ( var i = 0; i < ids.length; i++ ) {
+					cached = _.find( postSelectCache, _.matches( { id: ids[i] } ) );
+					if ( cached ) {
+						parsedData.push( cached );
+					}
+				}
+
+				// If not multiple - return single value if we have one.
+				if ( parsedData.length && ! self.model.get( 'multiple' ) ) {
+					callback( parsedData[0] );
+					return;
+				}
+
+				var uncachedIds = _.difference( ids, _.pluck( parsedData, 'id' ) );
+
+				if ( ! uncachedIds.length ) {
+
+					callback( parsedData );
+
+				} else {
+
+					var initAjaxData      = jQuery.extend( true, {}, ajaxData );
+					initAjaxData.action   = 'shortcode_ui_post_field';
+					initAjaxData.post__in = uncachedIds;
+
+					$.get( ajaxurl, initAjaxData ).done( function( response ) {
 
 						if ( ! response.success ) {
 							return { results: {}, more: false };
 						}
 
-						// Cache data for quicker rendering later.
 						postSelectCache = $.extend( postSelectCache, response.data.posts );
 
-						var more = ( page * response.data.posts_per_page ) < response.data.found_posts; // whether or not there are more results available
-						return { results: response.data.posts, more: more };
-
-					},
-				},
-
-				/**
-				 * Initialize Callback
-				 * Used to set render the initial value.
-				 * Has to make a request to get the title for the current ID.
-				 */
-				initSelection: function(element, callback) {
-
-					var ids, parsedData = [], cached;
-
-					// Convert stored value to array of IDs (int).
-					ids = $(element)
-						.val()
-						.split(',')
-						.map( function (str) { return str.trim(); } )
-						.map( function (str) { return parseInt( str ); } );
-
-					if ( ids.length < 1 ) {
-						return;
-					}
-
-					// Check if there is already cached data.
-					for ( var i = 0; i < ids.length; i++ ) {
-						cached = _.find( postSelectCache, _.matches( { id: ids[i] } ) );
-						if ( cached ) {
-							parsedData.push( cached );
+						// If not multi-select, expects single object, not array of objects.
+						if ( ! self.model.get( 'multiple' ) ) {
+							callback( response.data.posts[0] );
+							return;
 						}
-					}
 
-					// If not multiple - return single value if we have one.
-					if ( parsedData.length && ! self.model.get( 'multiple' ) ) {
-						callback( parsedData[0] );
-						return;
-					}
-
-					var uncachedIds = _.difference( ids, _.pluck( parsedData, 'id' ) );
-
-					if ( ! uncachedIds.length ) {
+						// Append new data to cached data.
+						// Sort by original order.
+						parsedData = parsedData
+							.concat( response.data.posts )
+							.sort(function (a, b) {
+								if ( ids.indexOf( a.id ) > ids.indexOf( b.id ) ) return 1;
+								if ( ids.indexOf( a.id ) < ids.indexOf( b.id ) ) return -1;
+								return 0;
+							});
 
 						callback( parsedData );
+						return;
 
-					} else {
+					} );
 
-						var initAjaxData      = jQuery.extend( true, {}, ajaxData );
-						initAjaxData.action   = 'shortcode_ui_post_field';
-						initAjaxData.post__in = uncachedIds;
+				}
 
-						$.get( ajaxurl, initAjaxData ).done( function( response ) {
+			},
 
-							if ( ! response.success ) {
-								return { results: {}, more: false };
-							}
+		} );
 
-							postSelectCache = $.extend( postSelectCache, response.data.posts );
-
-							// If not multi-select, expects single object, not array of objects.
-							if ( ! self.model.get( 'multiple' ) ) {
-								callback( response.data.posts[0] );
-								return;
-							}
-
-							// Append new data to cached data.
-							// Sort by original order.
-							parsedData = parsedData
-								.concat( response.data.posts )
-								.sort(function (a, b) {
-									if ( ids.indexOf( a.id ) > ids.indexOf( b.id ) ) return 1;
-									if ( ids.indexOf( a.id ) < ids.indexOf( b.id ) ) return -1;
-									return 0;
-								});
-
-							callback( parsedData );
-							return;
-
-						} );
-
-					}
-
-				},
-
-			} );
-
-			// Make multiple values sortable.
-			if ( this.model.get( 'multiple' ) ) {
-				$field.select2('container').find('ul.select2-choices').sortable({
-	    			containment: 'parent',
-	    			start: function() { $('.shortcode-ui-post-select').select2('onSortStart'); },
-	    			update: function() { $('.shortcode-ui-post-select').select2('onSortEnd'); }
-				});
-			}
-
-			return this;
-
+		// Make multiple values sortable.
+		if ( this.model.get( 'multiple' ) ) {
+			$field.select2('container').find('ul.select2-choices').sortable({
+				containment: 'parent',
+				start: function() { $('.shortcode-ui-post-select').select2('onSortStart'); },
+				update: function() { $('.shortcode-ui-post-select').select2('onSortEnd'); }
+			});
 		}
 
-	} );
+		return this;
 
-	/**
-	 * Extending SUI Media Controller to hide Select2 UI Drop-Down when menu
-	 * changes in Meida modal
-	 * 1. going back/forth between different shortcakes (refresh)
-	 * 2. changing the menu in left column (deactivate)
-	 * 3. @TODO closing the modal.
-	 */
-	var mediaController = sui.controllers.MediaController;
-	sui.controllers.MediaController = mediaController.extend({
+	}
 
-		refresh: function(){
-			mediaController.prototype.refresh.apply( this, arguments );
-			this.destroySelect2UI();
-		},
+} );
 
-		//doesn't need to call parent as it already an "abstract" method in parent to provide callback
-		deactivate: function() {
-			this.destroySelect2UI();
-		},
+module.exports = sui.views.editAttributeFieldPostSelect;
 
-		destroySelect2UI: function() {
-			$('.shortcode-ui-post-select.select2-container').select2( "close" );
-		}
+/**
+ * Extending SUI Media Controller to hide Select2 UI Drop-Down when menu
+ * changes in Meida modal
+ * 1. going back/forth between different shortcakes (refresh)
+ * 2. changing the menu in left column (deactivate)
+ * 3. @TODO closing the modal.
+ */
+var mediaController = sui.controllers.MediaController;
+sui.controllers.MediaController = mediaController.extend({
 
-	});
+	refresh: function(){
+		mediaController.prototype.refresh.apply( this, arguments );
+		this.destroySelect2UI();
+	},
 
-} )( jQuery );
+	//doesn't need to call parent as it already an "abstract" method in parent to provide callback
+	deactivate: function() {
+		this.destroySelect2UI();
+	},
+
+	destroySelect2UI: function() {
+		$('.shortcode-ui-post-select.select2-container').select2( "close" );
+	}
+
+});
