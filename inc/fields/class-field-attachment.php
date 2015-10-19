2aa99cc4da5acc52cfefa45cd968c4106c8affe0
Matth_eu <matthew@matth.eu>
2015-10-19 21:24:26 +0100

    Fix display of attachment details

--- inc/fields/class-field-attachment.php
+++ inc/fields/class-field-attachment.php
@@ -92,44 +92,51 @@ class Shortcake_Field_Attachment {
 		<script type="text/html" id="tmpl-fusion-shortcake-field-attachment">
 			<div class="field-block shortcode-ui-field-attachment shortcode-ui-attribute-{{ data.attr }}">
 				<label for="{{ data.attr }}">{{{ data.label }}}</label>
-				<button id="{{ data.attr }}" class="shortcake-attachment-select button button-small add">{{ data.addButton }}</button>
 				<# if ( typeof data.description == 'string' ) { #>
 					<p class="description">{{{ data.description }}}</p>
 				<# } #>
+				<button id="{{ data.attr }}" class="shortcake-attachment-select button button-small add">{{ data.addButton }}</button>
 				<div class="attachment-previews"></div>
 			</div>
 		</script>
 
 		<script type="text/html" id="tmpl-shortcake-image-preview">
 			<div class="shortcake-attachment-preview">
-				<div class="shortcake-attachment-preview-container attachment-preview attachment <# if ( ! data.sizes ) { #>loading<# } #>">
+				<div class="shortcake-attachment-preview-container attachment-preview attachment <# if ( data.type === 'image' && ! data.sizes ) { #>loading<# } #>">
 
 					<button class="button button-small remove" data-id="{{ data.id }}">×</button>
 
-					<div class="loading-indicator">
-						<span class="dashicons dashicons-format-image"></span>
-						<div class="attachment-preview-loading"><ins></ins></div>
-					</div>
-
-					<# if ( data.sizes ) { #>
+					<# if ( data.type === 'image' && data.sizes ) { #>
 						<div class="thumbnail">
 							<div class="centered">
-								<img src="{{ data.sizes.thumbnail.url }}" alt="" style="width: {{ data.sizes.thumbnail.width }}px; height: {{ data.sizes.thumbnail.height }}px;">
+								<img src="{{ data.sizes.thumbnail.url }}" alt="" style="width: {{ data.sizes.thumbnail.width }}px; height: {{ data.sizes.thumbnail.height }}px;" />
 							</div>
+						</div>
+					<# } else if ( data.type !== 'image' ) { #>
+						<div class="thumbnail">
+							<div class="centered">
+								<img src="{{ data.icon }}" />
+							</div>
+							<div class="filename"><div>{{ data.filename }}</div></div>
+						</div>
+					<# } else { #>
+						<div class="loading-indicator">
+							<span class="dashicons dashicons-format-image"></span>
+							<div class="attachment-preview-loading"><ins></ins></div>
 						</div>
 					<# } #>
 
 				</div>
 
 				<div class="thumbnail-details-container has-attachment">
-					<# if ( data.sizes ) { #>
-						<strong>Thumbnail Details</strong>
-						<div class="filename">{{ data.filename }}</div>
-						<div class="date-formatted">{{ data.dateFormatted }}</div>
-						<div class="size">{{ data.filesizeHumanReadable }}</div>
+					<strong>Thumbnail Details</strong>
+					<div class="filename">{{ data.filename }}</div>
+					<div class="date-formatted">{{ data.dateFormatted }}</div>
+					<div class="size">{{ data.filesizeHumanReadable }}</div>
+					<# if ( data.type === 'image' ) { #>
 						<div class="dimensions">{{ data.width }} &times; {{ data.height }}</div>
-						<div class="edit-link"><a href="{{ data.editLink }}">Edit Attachment</a></div>
 					<# } #>
+					<div class="edit-link"><a href="{{ data.editLink }}">Edit Attachment</a></div>
 				</div>
 			</div>
 		</script>
