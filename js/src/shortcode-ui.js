6113cec1847def08caca4c49916dd6f6bc772416
Nathaniel Taintor <goldenapplesdesign@gmail.com>
2015-11-18 13:48:11 -0500

    Add "Add Post Element" to media buttons
    Includes an "Add Post Element" to the media buttons array, which pops
    open the Insert Post Element frame of the media modal.
    
    props @ninetienne and @Vaince for code snippets in
    https://github.com/wp-shortcake/shortcake/issues/131

--- js/src/shortcode-ui.js
+++ js/src/shortcode-ui.js
@@ -24,4 +24,25 @@ $(document).ready(function(){
 		}
 	} );
 
+	$(document.body).on( 'click', '.shortcake-add-post-element', function( event ) {
+		var elem = $( event.currentTarget ),
+			editor = elem.data('editor'),
+			options = {
+				frame: 'post',
+				state: 'shortcode-ui',
+				title: shortcodeUIData.strings.media_frame_title
+			};
+
+		event.preventDefault();
+
+		// Remove focus from the `.insert-embed` button.
+		// Prevents Opera from showing the outline of the button
+		// above the modal.
+		//
+		// See: https://core.trac.wordpress.org/ticket/22445
+		elem.blur();
+
+		wp.media.editor.open( editor, options );
+	} );
+
 });
