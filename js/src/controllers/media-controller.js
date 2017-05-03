6d5d484459e6b5597fd05370621aa144e3f76195
Matthew Haines-Young <matthew@matth.eu>
2017-05-03 23:11:30 +0100

    Move render_edit action instead of a new action

--- js/src/controllers/media-controller.js
+++ js/src/controllers/media-controller.js
@@ -99,8 +99,17 @@ var MediaController = wp.media.controller.State.extend({
 				this.frame.mediaController.toggleSidebar( false );
 			}.bind( this ) );
 
-			var hookName = 'shortcode-ui.after_set_action_update';
-			wp.shortcake.hooks.doAction( hookName, currentShortcode );
+			/*
+			 * Action run after an edit shortcode overlay is rendered.
+			 *
+			 * Called as `shortcode-ui.render_edit`.
+			 *
+			 * @param shortcodeModel (object)
+			 *           Reference to the shortcode model used in this overlay.
+			 */
+			var hookName = 'shortcode-ui.render_edit';
+			var shortcodeModel = this.shortcodeModel;
+			wp.shortcake.hooks.doAction( hookName, shortcodeModel );
 
 		}.bind( this ) );
 
